3730174d28acb3395301b800b72202828d788fa1
florian <florian.mladitsch@gmail.com>
2018-06-18 18:33:39 +0200

    set translation to local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LANGUAGE } from './constants';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,10 @@ export class AppComponent {
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
     translate.use('en');
+
+    const language = localStorage.getItem(LANGUAGE);
+    if (language) {
+      translate.use(language);
+    }
   }
 }
--- src/app/components/user-settings.component.ts
+++ src/app/components/user-settings.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LANGUAGE } from '../constants';
 
 @Component({
   selector: 'app-user-settings',
   templateUrl: './user-settings.component.html'
 })
-export class UserSettingsComponent {
+export class UserSettingsComponent implements OnInit {
+  language = '';
+
+  constructor(private translate: TranslateService) {
+  }
+
+  ngOnInit() {
+    this.language = localStorage.getItem(LANGUAGE);
+    if (!this.language) {
+      this.language = 'en';
+    }
+  }
 
   saveSettings() {
-    console.log('saving...');
+    this.translate.use(this.language);
+    localStorage.setItem(LANGUAGE, this.language);
   }
 }
