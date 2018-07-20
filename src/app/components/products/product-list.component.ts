2efd2d5c79ebc02f02728f757612a89d013273e8
florian <florian.mladitsch@gmail.com>
2018-07-20 21:33:34 +0200

    table to list products and non-functional add button

--- src/app/components/products/product-list.component.ts
+++ src/app/components/products/product-list.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
 import { Product } from '../../services/model/Product';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent implements OnInit {
+  loading = false;
+  errorMessage = '';
+  products: Product[] = [];
 
   constructor(private service: BackendService) {
   }
 
   ngOnInit() {
+    this.loading = true;
+
     this.service.getProducts()
+      .pipe(finalize(() => {
+        this.loading = false;
+      }))
       .subscribe((products: Product[]) => {
-        console.log(products);
+        this.products = products;
       }, (error) => {
-        console.log(error);
+        this.errorMessage = error;
       });
   }
 }
