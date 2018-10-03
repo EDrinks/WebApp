8863ca5234864e4c3d9bb6332c7c3cf3cb164054
Florian Mladitsch <florian.mladitsch@gmail.com>
2018-10-03 16:08:30 +0200

    added chart.js added mock usage for chart js

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as chart from 'chart.js';
+import { BackendService } from '../../services/backend.service';
+import { finalize } from 'rxjs/operators';
+import { Product } from '../../services/model/Product';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html'
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
 
+  @ViewChild('myChart') myChart: ElementRef;
+
+  products: Product[] = [];
+  loadingProducts = false;
+  loadingProductsError = '';
+
+  constructor(private service: BackendService) {
+  }
+
+  ngOnInit() {
+    this.loadProducts();
+
+    const foo = new chart.Chart(this.myChart.nativeElement, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+  }
+
+  private loadProducts() {
+    this.loadingProducts = true;
+    this.loadingProductsError = '';
+
+    this.service.getProducts()
+      .pipe(finalize(() => {
+        this.loadingProducts = false;
+      }))
+      .subscribe((products: Product[]) => {
+        this.products = products;
+      }, (error) => {
+        this.loadingProductsError = error;
+      });
+  }
 }
