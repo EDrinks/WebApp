d24cee2622805841f449180feaa2452b488b8896
Florian Mladitsch <florian.mladitsch@gmail.com>
2018-10-03 17:40:40 +0200

    some styling for quick ordering

--- src/app/components/start.component.ts
+++ src/app/components/start.component.ts
@@ -3,6 +3,7 @@ import { BackendService } from '../services/backend.service';
 import { Tab } from '../services/model/Tab';
 import { finalize } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { PRIMARY_PRODUCT, QUICK_SELECT } from '../constants';
 
 @Component({
   selector: 'app-start',
@@ -14,15 +15,40 @@ export class StartComponent implements OnInit {
   tabsLoading = false;
   tabsError = '';
 
+  quickSelectProduct: string = null;
+  tabOrdersInProgress = {};
+  orderError = '';
+
   constructor(private service: BackendService, private router: Router) {
   }
 
   ngOnInit() {
     this.loadTabs();
+
+    if (localStorage.getItem(QUICK_SELECT) === '1') {
+      this.quickSelectProduct = localStorage.getItem(PRIMARY_PRODUCT);
+    }
   }
 
   selectTab(tab: Tab) {
     this.router.navigate(['tab', tab.id]);
+  }
+
+  quickOrder(tab: Tab) {
+    if (this.quickSelectProduct) {
+      this.tabOrdersInProgress[tab.id] = true;
+      this.orderError = '';
+
+      this.service.createOrder(tab.id, this.quickSelectProduct, 1)
+        .pipe(finalize(() => {
+          this.tabOrdersInProgress[tab.id] = false;
+        }))
+        .subscribe(() => {
+
+        }, (error) => {
+          this.orderError = error;
+        });
+    }
   }
 
   private loadTabs() {
