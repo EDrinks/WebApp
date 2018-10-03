eff89c177615f5824b72bfd131a50078c4d73d7a
Florian Mladitsch <florian.mladitsch@gmail.com>
2018-10-03 16:20:12 +0200

    settings mock for primary product and enable quick select

--- src/app/components/user-settings.component.ts
+++ src/app/components/user-settings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DATE_FORMAT, LANGUAGE } from '../constants';
+import { Product } from '../services/model/Product';
+import { BackendService } from '../services/backend.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-settings',
@@ -20,7 +23,19 @@ export class UserSettingsComponent implements OnInit {
     }
   ];
 
+  primaryProductId: string = null;
+  enableQuickSelect = false;
+
+  products: Product[] = [];
+  loadingProducts = false;
+  loadingProductsError = '';
+
+  constructor(private service: BackendService) {
+  }
+
   ngOnInit() {
+    this.loadProducts();
+
     let storedLanguage = localStorage.getItem(LANGUAGE);
     if (!storedLanguage) {
       storedLanguage = 'en';
@@ -40,5 +55,20 @@ export class UserSettingsComponent implements OnInit {
     localStorage.setItem(DATE_FORMAT, this.language.dateFormat);
 
     location.reload();
+  }
+
+  private loadProducts() {
+    this.loadingProducts = true;
+    this.loadingProductsError = '';
+
+    this.service.getProducts()
+      .pipe(finalize(() => {
+        this.loadingProducts = false;
+      }))
+      .subscribe((products: Product[]) => {
+        this.products = products;
+      }, (error) => {
+        this.loadingProductsError = error;
+      });
   }
 }
